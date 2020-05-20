406a431b1a1b665e2c34238ebad9a7b177b725f2
VayFull <40316778+VayFull@users.noreply.github.com>
2020-05-20 15:19:27 +0300

    tests

---
+++ Subs/Subs.Tests/BookApiTests.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing.Template;
+using Subs.Controllers;
+using Subs.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using Xunit;
+
+namespace Subs.Tests
+{
+    public class BookApiTests : IClassFixture<SharedDatabase>
+    {
+        public SharedDatabase Context { get; }
+        public BookApiTests(SharedDatabase context)
+        {
+            Context = context;
+        }
+
+        [Fact]
+        public async void Try_Get_Books_With_Random_Key()
+        {
+            using (var context = Context.CreateContext())
+            {
+                var controller = new BooksController(context);
+
+                var books = await controller.GetBooks("123");
+
+                Assert.False(books.Result is IEnumerable<Book>);
+            }
+        }
+
+        [Fact]
+        public async void Try_Get_Books_With_Success_Key()
+        {
+            using (var context = Context.CreateContext())
+            {
+                var controller = new BooksController(context);
+                var someUserKey = context.UserKeys.FirstOrDefault();
+                var key = "";
+                if (someUserKey != null)
+                {
+                    key = someUserKey.Key;
+                }
+
+                var books = await controller.GetBooks(key);
+
+                Assert.True(books.Value.Any());
+            }
+        }
+
+        [Fact]
+        public async void Try_Post_Book_Wrong_Book()
+        {
+            using (var context = Context.CreateContext())
+            {
+                var previousCount = context.Books.Count();
+                var controller = new BooksController(context);
+                var someWrongBook = new Book { Author = null, Name = "" };
+                await controller.PostBook(someWrongBook);
+                Assert.Equal(previousCount, context.Books.Count());
+            }
+        }
+
+        [Fact]
+        public async void Try_Post_Book_Right_Book()
+        {
+            using (var context = Context.CreateContext())
+            {
+                var previousCount = context.Books.Count();
+                var controller = new BooksController(context);
+                var someOkBook = new Book { Author = "someAuthor", Name = "someName", FileName = "someFileName", Id = 234 };
+                await controller.PostBook(someOkBook);
+                Assert.Equal(previousCount + 1, context.Books.Count());
+            }
+        }
+    }
+}
---
+++ Subs/Subs.Tests/SharedDatabase.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Npgsql;
+using Subs.Domain.Entities;
+using Subs.Infrastructure.Contexts;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Text;
+
+namespace Subs.Tests
+{
+    public class SharedDatabase:IDisposable
+    {
+        private static readonly object _lock = new object();
+        private static bool _databaseInitialized;
+
+        public SharedDatabase()
+        {
+            Connection = new NpgsqlConnection(@"Host=localhost;Port=5432;Database=subs;Username=postgres;Password=1234QWER+");
+            Seed();
+            Connection.Open();
+        }
+
+        public DbConnection Connection { get; }
+
+        public SubsDbContext CreateContext(DbTransaction transaction = null)
+        {
+            var context = new SubsDbContext(new DbContextOptionsBuilder<SubsDbContext>().UseNpgsql(Connection).Options);
+
+            if (transaction != null)
+            {
+                context.Database.UseTransaction(transaction);
+            }
+
+            return context;
+        }
+
+        private void Seed()
+        {
+            lock (_lock)
+            {
+                if (!_databaseInitialized)
+                {
+                    using (var context = CreateContext())
+                    {
+                        context.Database.EnsureDeleted();
+                        context.Database.EnsureCreated();
+
+                        var firstBook = new Book { Author = "Повар", FileName = "книга повара.txt", Id = 10, Name = "Книга повара" };
+                        var secondBook = new Book { Author = "Повар2", FileName = "книга повара2.txt", Id = 100, Name = "Книга повара2" };
+
+                        var key = new UserKey { Key = "somesuccesskey", Username = "someuser" };
+
+                        context.AddRange(firstBook, secondBook, key);
+
+                        context.SaveChanges();
+                    }
+
+                    _databaseInitialized = true;
+                }
+            }
+        }
+
+        public void Dispose() => Connection.Dispose();
+    }
+}
